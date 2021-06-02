56efeb4b8561b003e36512e7148375488b76f5cc
zhangdj <1240706960@qq.com>
2021-06-02 16:10:09 +0800

    1、emoji测试； 2、jvm相关测试；

--- src/main/java/com/neusiri/jvm/DeadLockTest.java
+++ src/main/java/com/neusiri/jvm/DeadLockTest.java
@@ -39,4 +39,8 @@ public class DeadLockTest {
         }).start();
 
     }
+
+
+
+
 }
--- src/main/java/com/neusiri/jvm/OOMTest.java
+++ src/main/java/com/neusiri/jvm/OOMTest.java
@@ -1,6 +1,8 @@
 package com.neusiri.jvm;
 
 import com.neusiri.lambda.User;
+import com.neusiri.private_test.A;
+import org.junit.Test;
 
 import java.time.Year;
 import java.util.ArrayList;
@@ -21,5 +23,27 @@ public class OOMTest {
             list.add(User.builder().userId(i++).userName(UUID.randomUUID().toString()).build());
             User.builder().userId(j--).build();
         }
+    }
+
+    @Test
+    public void fillHeapTest() throws Exception {
+        fillHeap();
+        System.gc();
+    }
+
+    /**
+     * JConsole监控
+     */
+    public void fillHeap() throws Exception {
+        List<OOMObject> list = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            Thread.sleep(50);
+            list.add(new OOMObject());
+        }
+    }
+
+
+    static class OOMObject {
+        public byte[] placeHolder = new byte[64 * 1024];
     }
 }
